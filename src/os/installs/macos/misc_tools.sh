205b08c0e16828d6d51d2249c27aca9fce1974b5
Josafe <josafebalili@gmail.com>
2024-06-15 09:16:02 +0800

    add composer

---
+++ src/os/installs/macos/misc_tools.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "../../utils.sh" \
+    && . "./utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+print_in_purple "\n   Miscellaneous Tools\n\n"
+
+brew_install "ShellCheck" "shellcheck"
+brew_install "Yarn" "yarn"
+brew_install  "Composer" "composer"
+brew_install "PHP" "php@8.2"
